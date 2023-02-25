1b2d4610e614beb0dddf6012fd20afdb439fa513
Leo Junyi Liu <junyili2@andrew.cmu.edu>
2023-02-25 00:35:24 -0500

    Added endorse function file This file makes the basic ts file of the endorse function

---
+++ src/posts/endorse.ts
@@ -0,0 +1,68 @@
+'use strict';
+
+const db = require('../database');
+const plugins = require('../plugins');
+
+module.exports = function (Posts) {
+    Posts.endorse = async function (pid, uid) {
+        return await toggleEndorse('endorse', pid, uid);
+    };
+
+    Posts.unendorse = async function (pid, uid) {
+        return await toggleEndorse('unendorse', pid, uid);
+    };
+
+    async function toggleEndorse(type, pid, uid) {
+        if (parseInt(uid, 10) <= 0) {
+            throw new Error('[[error:not-logged-in]]');
+        }
+
+        const isEndorsing = type === 'endorse';
+
+        const [postData, hasEndorsed] = await Promise.all([
+            Posts.getPostFields(pid, ['pid', 'uid']),
+            Posts.hasEndorsed(pid, uid),
+        ]);
+
+        if (isEndorsing && hasEndorsed) {
+            throw new Error('[[error:already-endorsed]]');
+        }
+
+        if (!isEndorsing && !hasEndorsed) {
+            throw new Error('[[error:already-unendorsed]]');
+        }
+
+        if (isEndorsing) {
+            await db.sortedSetAdd(`uid:${uid}:endorses`, Date.now(), pid);
+        } else {
+            await db.sortedSetRemove(`uid:${uid}:endorses`, pid);
+        }
+        await db[isEndorsing ? 'setAdd' : 'setRemove'](`pid:${pid}:users_endorsed`, uid);
+        postData.endorses = await db.setCount(`pid:${pid}:users_endorsed`);
+        await Posts.setPostField(pid, 'endorses', postData.endorses);
+
+        plugins.hooks.fire(`action:post.${type}`, {
+            pid: pid,
+            uid: uid,
+            owner: postData.uid,
+            current: hasEndorsed ? 'endorsed' : 'unendorsed',
+        });
+
+        return {
+            post: postData,
+            isEndorsed: isEndorsing,
+        };
+    }
+
+    Posts.hasEndorsed = async function (pid, uid) {
+        if (parseInt(uid, 10) <= 0) {
+            return Array.isArray(pid) ? pid.map(() => false) : false;
+        }
+
+        if (Array.isArray(pid)) {
+            const sets = pid.map(pid => `pid:${pid}:users_endorsed`);
+            return await db.isMemberOfSets(sets, uid);
+        }
+        return await db.isSetMember(`pid:${pid}:users_endorsed`, uid);
+    };
+};
